41654d3bcb18003849058feb5c0fe5012aff8b15
DESKTOP-TA4D2SG\david <dafrios@bancolombia.com.co>
2021-04-13 22:55:13 -0500

    proyecto base

---
+++ src/test/java/co/com/wolox/WChallenge/WChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.wolox.WChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
